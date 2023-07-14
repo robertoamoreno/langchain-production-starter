1f3c324306631763efdedfb8e28fcc35c1d95d99
robertoamoreno <robertom@redmantech.com>
2023-07-13 22:15:49 -0600

    Update 2_Manage.py

--- ui/pages/2_Manage.py
+++ ui/pages/2_Manage.py
@@ -49,7 +49,7 @@ i = 0
 
 if st.button("Add 10 more video's", type="primary"):
     videos = load_and_show_videos(instance)
-    index_youtube_channel(st.session_state.channel_url, len(videos), 50)
+    index_youtube_channel(st.session_state.channel_url, len(videos), 100)
     i = 0
 
 while True:
